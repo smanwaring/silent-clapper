78983dd9c62ff10ce2c04f9377c80136482e8446
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2017-01-18 15:35:24 -0500

    got socket rooms connect working, need to work on disconnect

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ const socketio = require('socket.io');
 const routes = require('./routes') ;
 const db = require('../db');
 
+//require in our models
 require('../db/models');
 
 server.on('request', app);
@@ -44,41 +45,40 @@ app.use((err, req, res, next) => {
 
 // creates a new connection server for web sockets and integrates it into our HTTP server 
 // this needs to be below the server.on('request', app) so that our 
-// express app takes precedence over our socket server for typical HTTP requests 
-var io = socketio(server);
+// express app takes precedence over our socket server for typical HTTP requests
+let io = socketio(server);
 
 // use socket server as an event emitter in order to listen for new connections
 io.on('connection', function(socket){
 
   let newRoom;
+  let count = 0;
 
     // listens to emit to join room
   socket.on('wantToJoinRoom', function(roomName) {
     console.log('joining room', roomName);
     newRoom = roomName;
     socket.join(newRoom);
+    count = socket.adapter.rooms[newRoom].length;
+    io.emit('connectionEvent', count);
   });
-
-  //receives the newly connected socket called for each browser that connects to our server
-  let count = (io.engine.clientsCount);
-  io.emit('connectionEvent', count);
 
   //event that runs anytime a socket disconnects
   socket.on('disconnect', function(){
-    count = (io.engine.clientsCount - 1);
-    io.emit('connectionEvent', count);
+    // count = socket.adapter.rooms[newRoom].length - 1;
+    // io.emit('connectionEvent', count);
     socket.leave(newRoom);
     socket.disconnect();
-  })
+  });
 
   // server is receiving click data from the client here 
   // so we want to broadcast that data to all other connected clients 
   socket.on('registerAction', function(icon){
     console.log("room I am emitting to", newRoom)
     io.to(newRoom).emit('showAction', icon);
-  })
+  });
 
-})
+});
 
 server.listen(1337, function () {
     console.log('The server is listening on port 1337!');
